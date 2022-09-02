55d7bd4f6eb9db0a052b9c16f4944a8353dd8c0b
Raza Husain <razahussain3301@gmail.com>
2022-09-03 00:49:56 +0530

    My React App first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react'
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+// import About from './components/About';
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
+
 
 function App() {
+  const [mode,setMode] = useState("light");
+  const [alert,setAlert] = useState(null);
+
+  const showAlert = (message,type) => {
+      setAlert({message:message,type:type})
+      setTimeout(() => {
+        setAlert(null)
+      }, 1500);
+  }
+
+  const toggleMode = () =>{
+    if(mode === "light"){
+      setMode("dark");
+      document.body.style.backgroundColor = "#41464b"
+      showAlert("Dark Mode Enabled", "success")
+    }else{
+      setMode("light");
+      document.body.style.backgroundColor = "white"
+      showAlert("Light Mode Enabled", "success")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <Navbar title = "TextUtils" mode ={mode} toggleMode={toggleMode}/>
+      <Alert alert = {alert}/>
+      <div className="container my-3">
+        <TextForm heading="Enter The Text to Analyze" mode={mode} showAlert = {showAlert}/>
+      </div>
+      {/* <BrowserRouter>
+        <Navbar title = "TextUtils" mode={mode} toggleMode={toggleMode}/>
+        <Alert alert = {alert}/>
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/" element={<TextForm heading="Enter The Text to Analyze" mode={mode} showAlert = {showAlert}/>}/>
+            <Route exact path="/about" element={<About/>}/>
+          </Routes>
+        </div>
+      </BrowserRouter> */}
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+export default function TextForm(props) {
+  const [text,setText] = useState("")
+
+
+  function wordCount(){
+    let count = text.length;
+    if(count===0){
+    }else{
+      count = text.trim().split(/\s+/).length;
+    }
+    return count;
+  }
+
+  function handletoUp(){
+    let newText = text.toUpperCase();
+    setText(newText)
+    props.showAlert("Converted to UpperCase","success")
+  }
+
+  function handletoLo(){
+    let newText = text.toLowerCase();
+    setText(newText)
+    props.showAlert("Converted to LowerCase","success")
+  }
+
+  function handletoCl(){
+    setText("")
+    props.showAlert("Cleared Box","success")
+  }
+
+  function handletobase46(){
+    setText(btoa(text));
+    props.showAlert("Converted to Base64 from String","success")
+  }
+
+  function handletoString(){
+    setText(atob(text));
+    props.showAlert("Converted to String From Base64","success")
+  }
+
+  function handleChange(event) {
+    setText(event.target.value)
+  }
+
+ 
+  return (
+    <>
+    <div className="container" style={{color:props.mode==="dark"?"white":"black"}}>
+      <h1>{props.heading}</h1>
+      <div className="mb-3">
+        <textarea className="form-control" id="mybox" rows="6" value={text} onChange={handleChange} style={{color:props.mode==="dark"?"white":"black",backgroundColor:props.mode==="dark"?"#4c5258":"white"}}></textarea>
+      </div>
+    <button type="submit" className="btn btn-primary mx-1 " onClick={handletoUp}>Convert To Uppercase</button>
+    <button type="submit" className="btn btn-primary mx-1" onClick={handletoLo}>Convert To Lowercase</button>
+    <button type="submit" className="btn btn-primary mx-1" onClick={handletoCl}>Clear Text</button>
+    <button type="submit" className="btn btn-primary mx-1" onClick={handletobase46}>Convert To Base64 from String</button>
+    <button type="submit" className="btn btn-primary mx-1" onClick={handletoString}>Convert To String from Base64</button>
+    </div>
+    <div className="container my-2" style={{color:props.mode==="dark"?"white":"black"}}>
+      <h2>Your text Summary</h2>
+      <p>{wordCount()} words and {text.length} characters</p>
+      <p>{0.008 * text.split(" ").length} Minutes to read </p>
+      <h2>Preview</h2>
+      <p>{text.length>0?text:"Enter something to Preview it here."}</p>
+    </div>
+    </>
+  )
+}
+
+TextForm.propTypes = {heading: PropTypes.string.isRequired}
